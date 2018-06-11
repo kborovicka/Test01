d84800da4c32152da7bfd37b82c6431a9ad03154
kborovicka <kborovicka@seznam.cz>
2018-06-11 09:19:53 +0200

    ADD Globalization

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,31 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
-            Console.ReadKey();  
+
+            string Name = "Pepe";
+            var Date = DateTime.Now;
+
+            string s = $"Hello, {Name}! Today is {Date.DayOfWeek}, it's {Date:HH:mm} now.";
+            Console.WriteLine(s);
+
+
+            double speedOfLight = 299792.458;
+            FormattableString message = $"The speed of light is {speedOfLight:N3} km/s.";
+
+            System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.GetCultureInfo("nl-NL");
+            string messageInCurrentCulture = message.ToString();
+
+            var specificCulture = System.Globalization.CultureInfo.GetCultureInfo("en-IN");
+            string messageInSpecificCulture = message.ToString(specificCulture);
+
+            string messageInInvariantCulture = FormattableString.Invariant(message);
+
+            Console.WriteLine($"{System.Globalization.CultureInfo.CurrentCulture,-10} {messageInCurrentCulture}");
+            Console.WriteLine($"{specificCulture,-10} {messageInSpecificCulture}");
+            Console.WriteLine($"{"Invariant",-10} {messageInInvariantCulture}");
+
+
+            Console.ReadKey();   
         }
     }
 }
